a552bb9b91d0ab8cbdf05f0770ca3b91980cf478
Anna <kazanok777@gmail.com>
2022-07-14 18:52:19 +0300

    added homework 17, basic dom

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+  const li = document.querySelectorAll('li').forEach((item,index) => {
+    if (!(index % 2)){
+      return item.style.backgroundColor = 'red';
+    }
+  });
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+  let li = document.querySelector('li');
+  while (li){
+    if (li.classList.contains('likedItem')){
+      return li.style.backgroundColor ='blue';
+    }
+    li = li.nextElementSibling;
+    console.log(li);
+  }
+};
